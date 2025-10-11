66cd764de20bf913ff9bc5adb9e264a0af8510e2
mitae2 <kjhmen2@gmail.com>
2025-10-12 03:30:50 +0900

    hotfix

--- src/api/axiosAuth.ts
+++ src/api/axiosAuth.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosAuth: AxiosInstance = axios.create({
-  baseURL: "http:/52.78.21.91:8080", // ✅ /auth 경로용
+  baseURL: "http://52.78.21.91:8080", // ✅ /auth 경로용
   headers: { "Content-Type": "application/json" },
 });
 
